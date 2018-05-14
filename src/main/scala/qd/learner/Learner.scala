f9b2b977d7dc457ae250b51ae0ebc285fb4a4623
Richard Zhang <rmzhang@seas.upenn.edu>
2018-05-14 13:25:19 -0400

    Initial implementation of squared error gradient descent

--- src/main/scala/qd/learner/Learner.scala
+++ src/main/scala/qd/learner/Learner.scala
@@ -22,7 +22,7 @@ class Learner(edb: Config, refOut: Config, p0: Program, random: Random) {
 
   private var state = LearnerState(TokenVec(tokens, random))
   def getState: LearnerState = state
-  def update(): Unit = { state = state.nextState }
+  def update(): Unit = { state = state.nextStateL2 }
 
   case class LearnerState(pos: TokenVec) {
     val p: Program = pos.reorient(p0)
@@ -73,6 +73,13 @@ class Learner(edb: Config, refOut: Config, p0: Program, random: Random) {
       numerator / s1D
     }
 
+    lazy val gradientL2: TokenVec = {
+      val numeratorVecs = for (rel <- outputRels.toSeq;
+                               (t, lt) <- refOut(rel).support)
+                          yield gradient(rel, t) * (out(rel)(t).toDouble - lt.toDouble)
+      numeratorVecs.foldLeft(TokenVec.zero(tokens))(_ + _)
+    }
+
     lazy val nextState: LearnerState = {
       val (score, grad) = if (s0 < s1) (s0, gradientS0) else (s1, gradientS1)
       // We want to make score 1
@@ -101,6 +108,33 @@ class Learner(edb: Config, refOut: Config, p0: Program, random: Random) {
       ans
     }
 
+    lazy val nextStateL2: LearnerState = {
+      val (score, grad) = (errorL2Total, gradientL2)
+      if (grad.abs == 0) { this }
+      val delta0 = grad.unit / grad.abs * errorL2Total
+
+      def newPos(delta: TokenVec) : TokenVec = pos + delta
+      def newPosLim(delta: TokenVec): TokenVec = newPos(delta).limitLower(0.0).limitUpper(1.0)
+
+      var (delta, ans) = (delta0, LearnerState(newPosLim(delta0)))
+      while (ans.score < this.score && delta.abs > 0.1) {
+        print(".")
+        delta = delta / 2
+        ans = LearnerState(newPosLim(delta))
+      }
+
+      println(s" grad: ${grad}")
+      println(s"  score: $score. s0: $s0. s1: $s1. |grad|: ${grad.abs}. numRules: ${ans.pos.count(_._2 > 0)}.")
+      /* println("Token, pos, gradS0, gradS1, grad, unit, delta, newPos, newPosLim")
+      for (t <- tokens.toSeq.sortBy(_.name.asInstanceOf[Int])) {
+        if (pos(t) < 1.0) {
+          println(s"$t, ${pos(t)}, ${gradientS0(t)}, ${gradientS1(t)}, ${grad(t)}, ${grad.unit(t)}, ${delta(t)}, ${newPos(t)}, ${newPosLim(t)}")
+        }
+      } */
+
+      ans
+    }
+
     lazy val settle: LearnerState = {
       val usefulTokens = for (rel <- outputRels; t <- out(rel).support; token <- t._2.prov.toSeq) yield token
       println(usefulTokens)
@@ -117,6 +151,11 @@ class Learner(edb: Config, refOut: Config, p0: Program, random: Random) {
       val vt = out(rel)(t).toDouble
       val ct = refOut(rel)(t).toDouble
       (vt - ct) * (vt - ct)
+    }
+
+    val errorL2Total = {
+      val errors = for (rel <- outputRels.toSeq; t <- out(rel).support) yield errorL2(rel, t._1)
+      errors.sum
     }
   }
 }
