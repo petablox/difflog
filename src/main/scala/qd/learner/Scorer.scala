ae48d40a4b48252f3b7ec0a325e8282f225fe87c
Mukund Raghothaman <rmukund@grw571-2.CIS.upenn.edu>
2018-05-15 16:04:48 -0400

    Major bugfix!

--- src/main/scala/qd/learner/Scorer.scala
+++ src/main/scala/qd/learner/Scorer.scala
@@ -20,7 +20,7 @@ class Scorer(edb: Config, refOut: Config) {
   // L2
 
   def gradL2(pos: TokenVec, out: Config): TokenVec = {
-    val numeratorVecs = for (rel <- outputRels.toSeq; t <- allTuples(rel))
+    val numeratorVecs = for (rel <- outputRels.toSeq; t <- allTuples(rel).toSeq)
                         yield gradient(pos, out, rel, t) * (out(rel)(t).toDouble - refOut(rel)(t).toDouble)
     numeratorVecs.foldLeft(TokenVec.zero(pos.keySet))(_ + _)
   }
@@ -28,7 +28,7 @@ class Scorer(edb: Config, refOut: Config) {
   def errorL2(out: Config): Double = outputRels.toSeq.map(rel => errorL2(out, rel)).sum
 
   def errorL2(out: Config, rel: Relation): Double = {
-    val allErrors = for (rel <- outputRels.toSeq; t <- allTuples(rel)) yield errorL2(out, rel, t)
+    val allErrors = for (rel <- outputRels.toSeq; t <- allTuples(rel).toSeq) yield errorL2(out, rel, t)
     allErrors.sum
   }
 
