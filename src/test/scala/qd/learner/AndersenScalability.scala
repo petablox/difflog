37fafd3243507475a7b237aa1b2b1f2d15293106
Kihong Heo <kheo@cis.upenn.edu>
2018-05-22 22:43:58 +0000

    scalability

--- src/test/scala/qd/learner/AndersenScalability.scala
+++ src/test/scala/qd/learner/AndersenScalability.scala
@@ -34,47 +34,47 @@ class AndersenScalability extends FunSuite {
   val x2: Variable = Variable("x2", heap)
   val x3: Variable = Variable("x3", heap)
 
-  val soup_pre: Set[Rule] = Set(
+  val soup_pre: Seq[Rule] = Seq(
     Rule(1, Value(0.5, Token(1)), pt(x0, x1), addr(x0, x1)),
-    Rule(2, Value(0.5, Token(2)), pt(x0, x1), assgn(x0, x1)),
+    Rule(7, Value(0.5, Token(7)), pt(x2, x1), assgn(x2, x0), pt(x0, x1)),
+    Rule(17, Value(0.5, Token(17)), pt(x3, x1), load(x3, x2), pt(x0, x1), pt(x2, x0)),
+    Rule(23, Value(0.5, Token(23)), pt(x3, x1), store(x2, x0), pt(x0, x1), pt(x2, x3)),
+    Rule(21, Value(0.5, Token(21)), pt(x3, x1), load(x2, x0), pt(x0, x1), pt(x3, x2)),
+    Rule(18, Value(0.5, Token(18)), pt(x3, x1), pt(x0, x1), pt(x2, x0), store(x3, x2)),
+    Rule(24, Value(0.5, Token(24)), pt(x3, x1), addr(x0, x1), pt(x2, x0), pt(x3, x2)),
     Rule(3, Value(0.5, Token(3)), pt(x0, x1), load(x0, x1)),
+    Rule(22, Value(0.5, Token(22)), pt(x3, x1), pt(x0, x1), pt(x3, x2), store(x2, x0)),
+    Rule(27, Value(0.5, Token(27)), pt(x3, x1), pt(x2, x0), pt(x3, x2), store(x0, x1)),
+    Rule(12, Value(0.5, Token(12)), pt(x2, x1), load(x0, x1), pt(x2, x0)),
+    Rule(8, Value(0.5, Token(8)), pt(x2, x1), load(x2, x0), pt(x0, x1)),
+    Rule(10, Value(0.5, Token(10)), pt(x2, x1), addr(x0, x1), pt(x2, x0)),
+    Rule(26, Value(0.5, Token(26)), pt(x3, x1), load(x0, x1), pt(x2, x0), pt(x3, x2)),
+    Rule(16, Value(0.5, Token(16)), pt(x3, x1), assgn(x3, x2), pt(x0, x1), pt(x2, x0)),
+    Rule(2, Value(0.5, Token(2)), pt(x0, x1), assgn(x0, x1)),
+    Rule(9, Value(0.5, Token(9)), pt(x2, x1), pt(x0, x1), store(x2, x0)),
+    Rule(25, Value(0.5, Token(25)), pt(x3, x1), assgn(x0, x1), pt(x2, x0), pt(x3, x2)),
+    Rule(20, Value(0.5, Token(20)), pt(x3, x1), assgn(x2, x0), pt(x0, x1), pt(x3, x2)),
+    Rule(11, Value(0.5, Token(11)), pt(x2, x1), assgn(x0, x1), pt(x2, x0)),
+    Rule(13, Value(0.5, Token(13)), pt(x2, x1), pt(x2, x0), store(x0, x1)),
+    Rule(19, Value(0.5, Token(19)), pt(x3, x1), addr(x2, x0), pt(x0, x1), pt(x3, x2)),
+    Rule(15, Value(0.5, Token(15)), pt(x3, x1), addr(x3, x2), pt(x0, x1), pt(x2, x0)),
     Rule(4, Value(0.5, Token(4)), pt(x0, x1), store(x0, x1)),
+    Rule(14, Value(0.5, Token(14)), pt(x3, x1), pt(x0, x1), pt(x2, x0), pt(x3, x2)),
     Rule(5, Value(0.5, Token(5)), pt(x2, x1), pt(x0, x1), pt(x2, x0)),
     Rule(6, Value(0.5, Token(6)), pt(x2, x1), addr(x2, x0), pt(x0, x1)),
-    Rule(7, Value(0.5, Token(7)), pt(x2, x1), assgn(x2, x0), pt(x0, x1)),
-    Rule(8, Value(0.5, Token(8)), pt(x2, x1), load(x2, x0), pt(x0, x1)),
-    Rule(9, Value(0.5, Token(9)), pt(x2, x1), pt(x0, x1), store(x2, x0)),
-    Rule(10, Value(0.5, Token(10)), pt(x2, x1), addr(x0, x1), pt(x2, x0)),
-    Rule(11, Value(0.5, Token(11)), pt(x2, x1), assgn(x0, x1), pt(x2, x0)),
-    Rule(12, Value(0.5, Token(12)), pt(x2, x1), load(x0, x1), pt(x2, x0)),
-    Rule(13, Value(0.5, Token(13)), pt(x2, x1), pt(x2, x0), store(x0, x1)),
-    Rule(14, Value(0.5, Token(14)), pt(x3, x1), pt(x0, x1), pt(x2, x0), pt(x3, x2)),
-    Rule(15, Value(0.5, Token(15)), pt(x3, x1), addr(x3, x2), pt(x0, x1), pt(x2, x0)),
-    Rule(16, Value(0.5, Token(16)), pt(x3, x1), assgn(x3, x2), pt(x0, x1), pt(x2, x0)),
-    Rule(17, Value(0.5, Token(17)), pt(x3, x1), load(x3, x2), pt(x0, x1), pt(x2, x0)),
-    Rule(18, Value(0.5, Token(18)), pt(x3, x1), pt(x0, x1), pt(x2, x0), store(x3, x2)),
-    Rule(19, Value(0.5, Token(19)), pt(x3, x1), addr(x2, x0), pt(x0, x1), pt(x3, x2)),
-    Rule(20, Value(0.5, Token(20)), pt(x3, x1), assgn(x2, x0), pt(x0, x1), pt(x3, x2)),
-    Rule(21, Value(0.5, Token(21)), pt(x3, x1), load(x2, x0), pt(x0, x1), pt(x3, x2)),
-    Rule(22, Value(0.5, Token(22)), pt(x3, x1), pt(x0, x1), pt(x3, x2), store(x2, x0)),
-    Rule(23, Value(0.5, Token(23)), pt(x3, x1), store(x2, x0), pt(x0, x1), pt(x2, x3)),
-    Rule(24, Value(0.5, Token(24)), pt(x3, x1), addr(x0, x1), pt(x2, x0), pt(x3, x2)),
-    Rule(25, Value(0.5, Token(25)), pt(x3, x1), assgn(x0, x1), pt(x2, x0), pt(x3, x2)),
-    Rule(26, Value(0.5, Token(26)), pt(x3, x1), load(x0, x1), pt(x2, x0), pt(x3, x2)),
-    Rule(27, Value(0.5, Token(27)), pt(x3, x1), pt(x2, x0), pt(x3, x2), store(x0, x1))
   )
 
   val expected: Set[Any] = Set(1, 7, 17, 23)
   val maxVarCount: Int = 20
   
   test(s"Scalability") {
-    for (nrules <- Range(10, 31)) {
+    for (nrules <- Range(4, 28)) {
       val startTime = System.nanoTime()
       val seed = Try(sys.env.getOrElse("RANDOM_SEED", "0").toInt).getOrElse(0)
       val random: Random = new Random(seed)
       println(s"$nrules rules selected")
       val soup = soup_pre.take(nrules).toSet
-      val p = Program(s"AndersenfScalability", soup)
+      val p = Program(s"AndersenScalability", soup)
       val learner = new Learner(edb, refOut, p, random)
       learner.learn(0.01, 500)
       val endTime = System.nanoTime()
