47c538bb1c7f6cc632160951ea4ae81acdf11a50
Mukund Raghothaman <rmukund@grw571-2.CIS.upenn.edu>
2018-08-30 02:53:47 -0400

    Updates

--- src/main/scala/qd/evaluator/TrieEvaluator.scala
+++ src/main/scala/qd/evaluator/TrieEvaluator.scala
@@ -1,6 +1,7 @@
 package qd
 package evaluator
 
+import scala.collection.immutable.Iterable
 import scala.collection.parallel.{ParMap, ParSeq}
 
 object TrieEvaluator extends Evaluator {
@@ -11,6 +12,40 @@ object TrieEvaluator extends Evaluator {
     while (state.changed) { state = immediateConsequence(state.nextEpoch) }
     state.config
   }
+
+  case class RuleTrie[T <: Value[T]](leaves: Set[Rule[T]], map: Map[Literal, RuleTrie[T]]) extends Iterable[Rule[T]] {
+
+    // Commented because the following check is too time-consuming
+    // require(map.forall { case (literal, trie) => trie.forall(_.body.contains(literal)) })
+
+    val numRules: Int = leaves.size + map.values.map(_.numRules).sum
+    override def iterator: Iterator[Rule[T]] = map.values.foldLeft(leaves.iterator)(_ ++ _.iterator)
+    val variables: Set[Variable] = {
+      val vs1 = leaves.flatMap(_.head.variables)
+      val vs2 = map.flatMap { case (l, t) => l.variables ++ t.variables }
+      vs1 ++ vs2
+    }
+
+    def +(rule: Rule[T]): RuleTrie[T] = {
+      def add(remainingLiterals: Seq[Literal], trie: RuleTrie[T]): RuleTrie[T] = {
+        if (remainingLiterals.isEmpty) RuleTrie(trie.leaves + rule, trie.map)
+        else {
+          val litHead = remainingLiterals.head
+          val litRest = remainingLiterals.tail
+          val subTrie = trie.map.getOrElse(litHead, RuleTrie())
+          RuleTrie(trie.leaves, trie.map + (litHead -> add(litRest, subTrie)))
+        }
+      }
+      add(rule.body.toSeq.sortBy(_.toString), this)
+    }
+
+  }
+
+  object RuleTrie {
+    def apply[T <: Value[T]](): RuleTrie[T] = RuleTrie(Set(), Map())
+    def apply[T <: Value[T]](rules: Iterable[Rule[T]]): RuleTrie[T] = rules.foldLeft(RuleTrie[T]())(_ + _)
+  }
+
 
   case class State[T <: Value[T]](trie: RuleTrie[T], config: Config[T], changed: Boolean)
                                  (implicit val vs: Semiring[T]) {
