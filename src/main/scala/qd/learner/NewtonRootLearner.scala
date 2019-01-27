5c563e6fe8bdfb5104852a7451b2408b469d8e93
Mukund Raghothaman <rmukund@grw571-2.CIS.upenn.edu>
2019-01-27 04:10:20 -0500

    Updates

--- src/main/scala/qd/learner/NewtonRootLearner.scala
+++ src/main/scala/qd/learner/NewtonRootLearner.scala
@@ -36,6 +36,21 @@ object NewtonRootLearner extends Learner {
     val solutionPointOpt = simplifyIfSolutionPoint(problem, evaluator, scorer, currState)
     solutionPointOpt.getOrElse {
       val State(currPos, _, currGrad, currLoss) = currState
+
+      /* // Repeatedly lower delta until the step is actually heading downwards
+      var delta = currGrad.unit * currLoss / currGrad.abs
+      var nextPos = (currPos - delta).clip(0.0, 1.0).clip(0.01, 0.99, currPos)
+      var nextState = State(problem, evaluator, scorer, nextPos)
+
+      while (nextState.loss > currState.loss) {
+        scribe.info(s"  Halving delta! ${delta.abs}")
+        delta = delta * 0.5
+        nextPos = (currPos - delta).clip(0.0, 1.0).clip(0.01, 0.99, currPos)
+        nextState = State(problem, evaluator, scorer, nextPos)
+      }
+
+      nextState */
+
       val delta = currGrad.unit * currLoss / currGrad.abs
       val nextPos = (currPos - delta).clip(0.0, 1.0).clip(0.01, 0.99, currPos)
       State(problem, evaluator, scorer, nextPos)
