c50dab2df710d3252e2c533bc21f8e1a8f83e807
Mukund Raghothaman <rmukund@grw571-2.CIS.upenn.edu>
2019-02-18 14:13:31 -0500

    Extractor: Initial commit. Untested.

---
+++ src/main/scala/qd/dgraph/Derivation.scala
@@ -0,0 +1,20 @@
+package qd
+package dgraph
+
+import qd.util.Contract
+
+sealed abstract class Derivation {
+  def conclusion: DTuple
+}
+case class EDB(conclusion: DTuple) extends Derivation
+case class Clause(conclusion: DTuple, rule: Rule, antecedents: IndexedSeq[DTuple]) extends Derivation {
+  Contract.require(conclusion.signature == rule.head.relation.signature)
+  Contract.require(rule.body.length == antecedents.length)
+  Contract.require(rule.body.indices.forall { i =>
+    rule.body(i).relation.signature == antecedents(i).signature
+  })
+}
+
+object Derivation {
+  type DGraph = Map[Relation, Map[DTuple, Set[Derivation]]]
+}
